bd01df7e018205071846b0af6e07e3c4d3c470c0
Amardeep <amardeepssgj@gmail.com>
2017-05-10 10:40:47 +0530

    added comments and log4j

--- js/indiaCensusAmardeep.js
+++ js/indiaCensusAmardeep.js
@@ -1,14 +1,20 @@
 const readline = require('readline');
 const fs = require('fs');
+const log4js = require('log4js');
 let convert = function(year) {
+	const logger = log4js.getLogger();
+	// registering csv file as input to readline interface
 	const rl = readline.createInterface({
 		input: fs.createReadStream('../inputdata/India2011.csv')
 	});
+	// checking year is a valid number
 	if (isNaN(year)) {
+		logger.error('Not a number');
 		throw new Error('Not a number');
 	}
 	let final = [];
 	let count = 0;
+	// fetching line by line and extracting needed data
 	rl.on('line', (line) => {
 		let arr = line.split(/,(?=(?:(?:[^"]*"){2})*[^"]*$)/);
 		if (arr[4] === 'Total' && arr[5] !== 'All ages') {
@@ -23,12 +29,15 @@ let convert = function(year) {
 			count = (count + 1) % 28;
 		}
 	});
+	// writing json file on close event
 	rl.on('close', () => {
 		let finalJson = JSON.stringify(final);
 		fs.writeFile('../outputdata/IndiaCensusAmardeep.json', finalJson, function(err) {
 			if (err) {
+				logger.error('Error in writing output file');
 				throw err;
 			}
+			logger.info('Closing input file after reading');
 		});
 	});
 	return 'JSON written successfully';
